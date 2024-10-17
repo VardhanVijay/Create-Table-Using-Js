7cfaccd96404db8f09dcefe5f8e12778b39f9e9b
Vijay Vardhan <118056692+VardhanVijay@users.noreply.github.com>
2024-10-17 18:52:05 +0530

    Add files via upload

---
+++ task.js
@@ -0,0 +1,40 @@
+const fo=document.getElementsByClassName("form");
+
+const tab=document.createElement("table");
+// tab.style.height="300px";
+// tab.style.width="300px";
+tab.style.border="2px solid black";
+
+const row=document.getElementById("row");
+const col=document.getElementById("col");
+
+const btn=document.getElementById("sub");
+let rowNum=0;
+let colNum=0;
+
+btn.addEventListener("click" ,(event)=>{
+    event.preventDefault();
+    colNum=col.value;
+    rowNum=row.value;
+    createTable(rowNum,colNum);
+})
+
+function createTable(rowNum,colNum){
+    for (let cRow = 0; cRow < rowNum; cRow++) {
+        const rowCreate=document.createElement("tr");
+        // rowCreate.style.height="15px";
+        // rowCreate.style.width="100px";
+        // rowCreate.style.border="5px solid black";
+        for (let cCol = 0; cCol < colNum; cCol++) {
+            const data=document.createElement("td")
+            data.style.textAlign="center";
+            data.textContent="Explorin";
+            data.style.height="50px";
+            data.style.width="300px";
+            data.style.border="2px solid black";
+            rowCreate.appendChild(data);
+        }
+        tab.appendChild(rowCreate);
+    }
+    document.getElementById("tablediv").appendChild(tab);
+}
